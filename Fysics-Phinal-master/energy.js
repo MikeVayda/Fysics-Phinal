cf20a457e727f179a45da341e7e599f890196dde
Mike Vayda <9mvayda@stpatrick.org>
2017-05-16 10:53:19 -0500

    Getting it going!!!!!!!!!!!!!!!!!!!!

---
+++ Fysics-Phinal-master/energy.js
@@ -0,0 +1,17 @@
+var exports = module.exports = {};
+
+//use this prototype to create the other functions
+exports.kineticEnergy = function(m,v){};
+let ke =50 ;
+ke += 50{c
+    ke=ke/(v*v)
+    return ke;
+    let m =100
+    m += 100 * {c
+        m = m / (v*v)
+        return m;
+        let v = 20 ;
+        v *= 2*ke;
+        v= v/m;
+        v = /v ;
+        return v ;
---
+++ Fysics-Phinal-master/motion.js
@@ -0,0 +1,20 @@
+var exports = module.exports = {};
+
+//use this prototype to create the other functions
+exports.distance = function(v,t){};
+let distance (v,t){
+return d;
+function distance (a,v,t){
+    d=0.5*a*t*t+v+5;
+    function velocity
+    v*t*v*v*t+t*V;
+    return; v
+    function time
+    let time div;
+    return; t
+    function force
+    let force div;
+    return; f
+    function gravity
+    let gravity div;
+    return; g
